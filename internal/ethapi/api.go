bc07a59e9f46b89a0130a55ca8dfec3be9785e28
Виталий Кисель <kisel@internet.ru>
2024-02-17 14:49:54 +0300

    test

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2104,7 +2104,6 @@ func AccessList(ctx context.Context, b Backend, db *state.StateDB, header *types
 	if args.AccessList != nil {
 		prevTracer = logger.NewAccessListTracer(*args.AccessList, args.from(), to, precompiles)
 	}
-
 	for {
 		// Retrieve the current access list to expand
 		accessList := prevTracer.AccessList()
@@ -2125,6 +2124,8 @@ func AccessList(ctx context.Context, b Backend, db *state.StateDB, header *types
 		if err != nil {
 			return nil, 0, nil, err
 		}
+
+		fmt.Printf("nonce after %d", db.GetNonce(vm.AccountRef(msg.From).Address()))
 
 		// Apply the transaction with the access list tracer
 		tracer := logger.NewAccessListTracer(accessList, args.from(), to, precompiles)
